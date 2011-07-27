0e37ccd4e0a6fb7d88644fe90e9f9bbd402504c2
Angelo DiNardi <angelo@dinardi.name>
2011-07-27 01:26:54 -0700

    Save the accounts after editing is finished.

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -349,6 +349,19 @@
 
 	// I would rather just update the cell with an issue, but hey.
 	[self.tableView reloadData];
+    
+    NSManagedObjectContext *context = [fetchedResultsController managedObjectContext];
+    NSError *error;
+    if (![context save:&error]) {
+        /*
+         Replace this implementation with code to handle the error appropriately.
+         
+         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
+         */
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+
 }
 
 - (void) showAccountsNavItems {
